91c16a613bee1f406ca2b66bf347c0aeb02521dc
meriszecic <zecicmeris@gmail.com>
2023-06-25 17:31:20 +0200

    Adding plans

---
+++ todo-app/src/components/Plans.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+import Rekreativno from "./Rekreativno"
+import Studiranje from "./Studiranje"
+
+export default function Stundes() {
+   
+    useEffect(() => {
+
+    })
+
+    return (
+    <>
+    <Rekreativno></Rekreativno>
+    <Studiranje></Studiranje>
+    </>
+   )
+}
