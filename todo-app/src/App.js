a6749f98d8a4098b6b1eea58bf746f28f0b6986f
meriszecic <zecicmeris@gmail.com>
2023-06-25 17:32:10 +0200

    Updating App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Studiranje from './components/Studiranje';
+import Rekreativno from './components/Rekreativno';
+import Plans from './components/Plans'
+import DetailsOfStuding from './components/DetailsOfStuding';
 
-export default function App() {
+function App() {
   return (
     <>
-      
+    <Router>
+      <ul>
+        <li><Link to='studiranje'>Studiranje</Link></li>
+      </ul>
+      <ul>
+        <li><Link to='rekreacija'>Rekreacija</Link></li>
+      </ul>
+      <ul>
+        <li><Link to='plans'>Todos</Link></li>
+      </ul>
+    <Routes>
+      <Route path='studiranje' element={<Studiranje></Studiranje>}></Route>
+      <Route path='rekreacija' element={<Rekreativno></Rekreativno>}></Route>
+      <Route path='/studies/:id' element={<DetailsOfStuding></DetailsOfStuding>}></Route>
+      <Route path='plans' element={<Plans></Plans>}></Route>
+    </Routes>
+    </Router>
+    
     </>
   );
 }
 
+export default App;
